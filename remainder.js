1af453ad8ae8a15714a64d4593be06fef158c871
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:11:20 -0400

    Update remainder.js

--- remainder.js
+++ remainder.js
@@ -1,1 +1,12 @@
 
+/*
+remainder.js
+Create two variables that each store a positive number (name them anything you want, any positive numbers will do).
+
+Log out how the remainder that is left over when the first number is divided by the second number (i.e.: "the remainder of 7 divided by 3 is 1")
+*/
+
+let num1 = 9
+let num2 = 2
+
+console.log(`${remainder = num1 % num2}`)
