531a725f0ec62927ae7964adfc510b14313ef6dd
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:02:24 -0400

    Create adder.js

---
+++ adder.js
@@ -0,0 +1,14 @@
+/*adder.js
+Create two variables, num1 and num2. Store any numbers in the variables.
+
+Below this, add num1 and num2 together and store the result in a new variable (name it whatever you want).
+
+Finlly, log out num1, num2, and sum in a human readable sentence (i.e.: "8 and 5 add to 13").
+*/
+
+let num1 = 8
+let num2 = 5
+
+let sum = num1 + num2
+
+console.log(`${num1} and ${num2} add to ${sum}`)
