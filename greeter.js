8f41ca85d27a3a18ce6cd061465230966b0f1728
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:07:59 -0400

    Update greeter.js

--- greeter.js
+++ greeter.js
@@ -1,1 +1,14 @@
+/*
+greeter.js
+Create a variable myName and store your name in it.
+
+Below this, write a line of code that displays some greeting to that name (i.e.: "Hello Barry!")
+
+*/
+
+let myName = "Charles.";
+
+
+
+console.log(`Welcome!,my name is ${myName}`)
 
