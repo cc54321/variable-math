55319defe1c78ddde76b67ac4e632d5569a17757
cc54321 <112970207+cc54321@users.noreply.github.com>
2022-10-08 12:06:19 -0400

    Update future-age.js

--- future-age.js
+++ future-age.js
@@ -1,7 +1,8 @@
 
 /*
 future-age.js
-Create a variable called age and set its value to your age. Now, write another line of code that logs out a sentence telling you how old you will be in five years.
+Create a variable called age and set its value to your age. Now, write another 
+line of code that logs out a sentence telling you how old you will be in five years.
 */
 
 
